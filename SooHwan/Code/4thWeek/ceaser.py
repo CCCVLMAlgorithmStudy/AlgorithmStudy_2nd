1571e8e58d56724001c722605718ae6db196c0df
SooHwanCho <chh0107@gmail.com>
2019-05-30 14:04:52 +0900

    SooHwan's 4thWeek Code&Solution(6/6) - refine ceaser.py

--- SooHwan/Code/4thWeek/ceaser.py
+++ SooHwan/Code/4thWeek/ceaser.py
@@ -2,13 +2,9 @@ def solution(s, n):
     answer = ''
     Lowercharacters = 'abcdefghijklmnopqrstuvwxyz'
     Uppercharacters = Lowercharacters.upper()
-    for c in s:
-        if c in Lowercharacters:
-            answer+= Lowercharacters[(Lowercharacters.index(c)+n)%26]
-        elif c in Uppercharacters:
-             answer+= Uppercharacters[(Uppercharacters.index(c)+n)%26]
-        else:
-            answer+=c
+    
+    table = str.maketrans(Lowercharacters+Uppercharacters,Lowercharacters[n:]+Lowercharacters[:n]+Uppercharacters[n:]+Uppercharacters[:n])
+    answer = s.translate(table)
     return answer
 
 
